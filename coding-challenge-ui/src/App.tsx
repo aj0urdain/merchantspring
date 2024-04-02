c3f5a7ff5d3db1b58a1cdcf571eac5094313908e
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-04-02 11:59:03 +1100

    Modal Update

--- coding-challenge-ui/src/App.tsx
+++ coding-challenge-ui/src/App.tsx
@@ -5,6 +5,7 @@ import FlagIcon from "./components/flagIcon";
 
 import "rc-pagination/assets/index.css";
 import Controls from "./components/widget/controls";
+import { Modal } from "./components/modal";
 
 const AppWrapper = styled.div`
   height: 100vh;
@@ -248,75 +249,6 @@ interface Order {
   shopName: string;
   country: string;
 }
-
-interface ModalProps {
-  onClose: () => void;
-  children: React.ReactNode;
-  retryFetch: () => void;
-}
-
-const ModalContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.5);
-`;
-
-const ModalContent = styled.div`
-  background-color: white;
-  padding: 1rem;
-  border-radius: 1rem;
-
-  position: relative;
-  width: 100%;
-  height: 100%;
-  max-width: 600px;
-  max-height: 20vh;
-`;
-
-const ModalHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const ModalTitle = styled.h2`
-  font-size: 1.5rem;
-  color: #5e5e5e;
-`;
-
-const ModalMessageContainer = styled.div`
-  margin: 1rem 0;
-`;
-
-const ModalSubmit = styled.button`
-  background-color: #007bff;
-  color: white;
-  padding: 0.5rem 1rem;
-  border: none;
-  border-radius: 0.5rem;
-  cursor: pointer;
-`;
-
-const Modal: React.FC<ModalProps> = ({ onClose, children, retryFetch }) => (
-  <ModalContainer>
-    {/* Use inline styles or a className to show the modal */}
-    <ModalContent>
-      <ModalHeader>
-        <ModalTitle>API Connection Error</ModalTitle>
-      </ModalHeader>
-      <ModalMessageContainer></ModalMessageContainer>
-
-      {children}
-      <ModalSubmit onClick={() => retryFetch()}>Retry Connection</ModalSubmit>
-    </ModalContent>
-  </ModalContainer>
-);
 
 const App = () => {
   const navigate = useNavigate();
--- coding-challenge-ui/src/components/modal/index.tsx
+++ coding-challenge-ui/src/components/modal/index.tsx
@@ -1,7 +1,74 @@
 import React from "react";
+import styled from "styled-components";
 
-const Modal = () => {
-  return <div>Modal</div>;
-};
+interface ModalProps {
+  onClose: () => void;
+  children: React.ReactNode;
+  retryFetch: () => void;
+}
 
-export default Modal;
+const ModalContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ModalContent = styled.div`
+  background-color: white;
+  padding: 1rem;
+  border-radius: 1rem;
+
+  position: relative;
+  width: 100%;
+  height: 100%;
+  max-width: 600px;
+  max-height: 20vh;
+`;
+
+const ModalHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ModalTitle = styled.h2`
+  font-size: 1.5rem;
+  color: #5e5e5e;
+`;
+
+const ModalMessageContainer = styled.div`
+  margin: 1rem 0;
+`;
+
+const ModalSubmit = styled.button`
+  background-color: #007bff;
+  color: white;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+`;
+
+export const Modal: React.FC<ModalProps> = ({
+  onClose,
+  children,
+  retryFetch,
+}) => (
+  <ModalContainer>
+    <ModalContent>
+      <ModalHeader>
+        <ModalTitle>API Connection Error</ModalTitle>
+      </ModalHeader>
+      <ModalMessageContainer></ModalMessageContainer>
+
+      {children}
+      <ModalSubmit onClick={() => retryFetch()}>Retry Connection</ModalSubmit>
+    </ModalContent>
+  </ModalContainer>
+);
